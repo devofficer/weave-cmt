51789250392a953ef463366686576461948d4e6e
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gmail.com>
2021-11-14 21:59:18 +0530

    added factory interface"

--- contracts/stake/stake.sol
+++ contracts/stake/stake.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-import {IPancakeRouter02,IPancakeFactory} from './pancakeRouter.sol';
+import {IPancakeRouter02,IPancakeFactory} from "./pancakeRouter.sol";
 
     contract Stake is Ownable {
         using SafeMath for uint256;
